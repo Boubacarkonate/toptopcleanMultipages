05c787f38dd679ef6e40d1c6a202f6d75a458052
Bouba <boubacar.konate@outlook.fr>
2025-11-14 00:52:31 +0100

    fix(multipages) : styles

--- main.js
+++ main.js
@@ -12,3 +12,52 @@ document.querySelectorAll('.fade-up').forEach(el => io.observe(el));
 
 // Année dynamique dans le footer
 document.getElementById('year').textContent = new Date().getFullYear();
+
+ const images = [
+        'images/nettoyage-hotte-professionnelle-restaurant.webp',
+        'images/personne-qui-s-occupe-du-nettoyage-des-bureaux.jpg',
+        'images/chantier.jpg',
+
+    ];
+
+   const circle = document.querySelector('.hero-image-circle');
+    const INTERVAL = 6000;
+    let idx = 0;
+
+    function changeImage() {
+        // Crée un nouvel élément img pour la transition
+        const newImg = document.createElement('img');
+        newImg.src = images[idx];
+        newImg.style.opacity = '0';
+        newImg.style.animation = 'kenburns 6s ease-in-out forwards';
+        circle.appendChild(newImg);
+
+        // Fade-in
+        setTimeout(() => { newImg.style.opacity = '1'; }, 50);
+
+        // Supprime l'ancien img après fade
+        const oldImg = circle.querySelectorAll('img');
+        if (oldImg.length > 1) {
+            setTimeout(() => {
+                oldImg[0].remove();
+            }, 1200); // correspond à transition opacity
+        }
+
+        // Prépare l'index pour la prochaine image
+        idx = (idx + 1) % images.length;
+    }
+
+    // Lance la première image
+    changeImage();
+    setInterval(changeImage, INTERVAL);
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.site-header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled'); // ajoute fond noir + texte blanc
+    } else {
+        header.classList.remove('scrolled'); // revient au fond blanc + texte noir
+    }
+});
+
+
